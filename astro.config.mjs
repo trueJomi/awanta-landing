5e0816cdacba2f01ee99d0e6c45abec9ef663fa3
Jose Miguel Vasquez <pixgel13040@gmail.com>
2025-08-02 17:16:33 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,25 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://awanta.app',
+  output: "server",
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [react()],
   compressHTML: true,
+
   build: {
     inlineStylesheets: 'auto'
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
